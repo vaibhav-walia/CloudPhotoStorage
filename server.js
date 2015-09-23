ec16ea5edcc47435500685bf90000ad0780aa2ba
vaibhav walia <walia.vaibhav@yahoo.com>
2015-09-23 08:57:00 +0000

    Connecting to mongodb, mongod has to be running first

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+
 function start(port,route,handle){
-	var http = require("http");
-	var url = require("url");
-        var mongoClient = require('mongodb').MongoClient;
+        var http = require("http"),
+        mongodb = require('mongodb'),
+        url = require("url");
+        var dbServer = new mongodb.Server('127.0.0.1',27017);
+ 
+new mongodb.Db('CloudPhotoStorage',dbServer).open(function(err,client){
+       if(err){
+          throw err;
+       }
+       else{
+          console.log('connected to mongodb');
+          http.createServer(function(request,response){
+          var path = url.parse(request.url).pathname;
+          console.log("request for "+path+" received");
+          route(handle,path);
+          response.writeHead(200,{"Content-Type":"text/plain"});
+          response.write("It Works!");
+          response.end();
+          }).listen(port);
 
-	http.createServer(function(request,response){
-		var path = url.parse(request.url).pathname;
-		console.log("request for "+path+" received");
-		route(handle,path);
-		response.writeHead(200,{"Content-Type":"text/plain"});
-		response.write("It Works!");
-		response.end();
-	}).listen(port);
-	console.log("server has started listening on port "+port);
+          console.log("server has started listening on port "+port);
+       } 
+});
 }
-
-exports.start = start;
+exports.start = start;                          
